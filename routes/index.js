7b4d9dae75c7c786ac76e2d427ee9b4c30b8c85e
megastar98 <obikoya11@gmail.com>
2017-08-07 12:10:39 +0100

    first commit

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+var multer = require('multer');
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null,__dirname, '/uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now())
+  }
+})
+ 
+var upload = multer({ storage: storage })
+ 
+
+router.post('/upload',upload.single('picture'),function(req,res,next){
+  var dir = req.file.path;
+  var db = req.db;
+  
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
